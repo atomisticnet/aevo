9c9ca95eb52a1cb9aa0be264f7ef9d557ed6d4a0
Alexander Urban <aurban@atomistic.net>
2020-05-29 22:44:48 -0400

    Package renamed to 'aevo'

--- setup.py
+++ setup.py
@@ -1,30 +1,43 @@
 from setuptools import setup, find_packages
-from numpy import get_include
+from codecs import open
+import os
+import glob
 
-with open("README.rst") as f:
-    long_desc = f.read()
-    ind = long_desc.find("\n")
-    long_desc = long_desc[ind + 1:]
+__author__ = "Alexander Urban"
+__email__ = "aurban@atomistic.net"
+
+here = os.path.abspath(os.path.dirname(__file__))
+package_name = 'aevo'
+package_description = 'Atomistic evolution'
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as fp:
+    long_description = fp.read()
+
+# Get version number from the VERSION file
+with open(os.path.join(here, package_name, 'VERSION')) as fp:
+    version = fp.read().strip()
 
 setup(
-    name="pygs",
+    name=package_name,
     packages=find_packages(),
-    version="0.1.0a1",
+    version=version,
     install_requires=["numpy>=1.5"],
-    author="Alexander Urban",
-    author_email="aurban@lbl.gov",
-    maintainer="Alexander Urban",
-    license="MIT",
-    description="A collection of tools for the pymatgen library.",
-    long_description=long_desc,
+    author=__author__,
+    author_email=__email__,
+    maintainer=__author__,
+    license="MPL2",
+    description=package_description,
+    long_description=long_description,
     classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Scientific/Engineering :: Chemistry",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
     ],
-    include_dirs=[get_include()]
+    scripts=glob.glob(os.path.join("scripts", "*.py"))
 )
